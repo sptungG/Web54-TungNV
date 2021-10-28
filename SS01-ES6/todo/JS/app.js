ba9e8ea00e5f117062df4d3a606a5ed421fe98ad
sptungG <ngoviettung154@gmail.com>
2021-10-28 10:34:17 +0700

    update path

--- SS01-ES6/todo/JS/app.js
+++ SS01-ES6/todo/JS/app.js
@@ -2,8 +2,8 @@
 // Model: xử lý chức năng về logic Thêm, Xóa, Sửa
 // View: hiển thị DOM, ...
 // Controller: gắn kết Model, View, thực hiện những cái liên quan đến User
-import Controller from "./controller.js";
-import Model from "./model.js";
-import View from "./view.js";
+import Controller from "./Controller.js";
+import Model from "./Model.js";
+import View from "./View.js";
 
 const app = new Controller(new Model(), new View());
---
+++ SS01-ES6/todo/README.md
@@ -0,0 +1,5 @@
+## DEMO
+### DEMO
+#### DEMO
+
+**[Web54-TungNV/SS01-ES6/todo](https://sptungg.github.io/Web54-TungNV/SS01-ES6/todo)**
