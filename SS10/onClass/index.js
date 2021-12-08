a469b2092032723d39788316ff11acb888c5c5b6
sptungG <ngoviettung154@gmail.com>
2021-12-08 09:28:15 +0700

    update

---
+++ SS10/onClass/index.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+const express = require('express');
+require('express-async-errors');
+const mongoose = require('mongoose');
+const PostRouter = require('./modules/post');
+const CommentRouter = require('./modules/comment');
+const AuthRouter = require('./modules/auth');
+const UploadRouter = require('./modules/upload');
+
+const log = require('./common/middlewares/log');
+const errorHandler = require('./common/errorHandler');
+
+async function main() {
+  await mongoose.connect(process.env.MONGODB_URI)
+
+  console.log('Mongodb connected');
+  const app = express();
+
+  app.use(log)
+  app.use(express.json());
+
+  app.use('/uploads', express.static('uploads'));
+
+  app.use('/api/posts', PostRouter);
+  app.use('/api/comments', CommentRouter);
+  app.use('/api/auth', AuthRouter);
+  app.use('/api/upload', UploadRouter);
+
+  app.use(errorHandler);
+
+  app.listen(process.env.PORT || 9000, (err) => {
+    if (err) throw err;
+
+    console.log('Server connected');
+  });
+}
+
+main();
